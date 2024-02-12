3f8f6e3f2f42570e78cfc6b93cec8c0b21b158ab
TY_CARLTON <tyrellcarlton@gmail.com>
2024-02-12 15:04:51 -0800

    need to work on fixing the hamburger menu, and getting navigation working correctly

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,38 +1,51 @@
 import React, { useState } from "react";
-import { Link, Route } from "react-router-dom";
-import About from "../components/About";
-import Archive from "../components/Archive.jsx";
-import Home from "../components/Home.jsx";
-import  LatestPosts  from "./LatestPosts.jsx";
-//Toggle button below
+import { Link, Routes, Route } from 'react-router-dom';
+import About from "./About";
+import Archive from "./Archive";
+import Home from "./Home";
+import LatestPosts from "./LatestPosts";
 
 const Nav = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
-  ///nav items
   return (
-      
-    <nav className={`navbar ${menuOpen ? "open" : ""}`}>
-      <div className='menu-icon' onClick={toggleMenu}>
-        <div className='bar'></div>
-        <div className='bar'></div>
-        <div className='bar'></div>
-      </div>
-      <div>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/latestPosts">Latest Posts</Link></li>
-          <li><Link to="/archive">Archive</Link></li>
-        </ul>
-      </div>
-    </nav>
-   
-);
-}
-
+    <header className="header">
+      <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+        <div className="menu-icon" onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+        <a href="#" className="nav-logo" color="purple"></a>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/latestPosts">Latest Posts</Link>
+            </li>
+            <li>
+              <Link to="/archive">Archive</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Routes>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/latestPosts" component={LatestPosts} />
+        <Route path="/archive" component={Archive} />
+      </Routes>
+    </header>
+  );
+};
 
 export default Nav;
